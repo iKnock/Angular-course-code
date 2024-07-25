123c078920a5f315aec5f6c37cd7139990510d58
iKnock <ergana.henok@gmail.com>
2024-07-25 12:33:15 +0200

    first temp and comp

---
+++ project-one/appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../modles/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent {
+
+  newAppointmentTitle: string = "";
+  newAppointmentDate: Date = new Date();
+  appointment: Appointment[] = []
+}
